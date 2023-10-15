571cc3faa60b8cb80d6fcf9ba5fa21433b037478
joao-miranda <joao.miranda@tatic.net>
2023-10-14 21:01:22 -0300

    first commit

---
+++ quotation/src/main/java/org/br/mining/entity/QuotationEntity.java
@@ -0,0 +1,29 @@
+package org.br.mining.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "quotation")
+@Data
+@NoArgsConstructor
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime date;
+
+    @Column(name = "currency_price")
+    private BigDecimal currencyPrice;
+
+    @Column(name = "pct_change")
+    private String pctChange;
+
+    private String pair;
+}
