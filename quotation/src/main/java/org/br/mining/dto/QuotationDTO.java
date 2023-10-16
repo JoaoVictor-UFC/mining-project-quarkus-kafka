dacbbec402d7a9acd28513e519a5b6bda5340e64
joao-miranda <joao.miranda@tatic.net>
2023-10-16 17:38:22 -0300

    implementing communication with the external service

---
+++ quotation/src/main/java/org/br/mining/dto/QuotationDTO.java
@@ -0,0 +1,20 @@
+package org.br.mining.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Jacksonized
+@Data
+@Builder
+@AllArgsConstructor
+public class QuotationDTO {
+
+    private LocalDateTime date;
+
+    private BigDecimal currencyPrice;
+}
